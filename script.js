e9b45cd9d59a2ce8f475584bc350fdab03974570
Adam Ibrahim <muhadamibrahim1873@gmail.com>
2023-03-08 20:53:10 +0700

    dmzx

---
+++ script.js
@@ -0,0 +1,11 @@
+flag = 1;
+function f1() {
+    alert("ditunggu traktirannya yaa");
+    alert("wkwk candaa kok");
+    alert("maap yaa ga bisa ngasih hadiah");
+    alert("ayo meet up aja");
+    alert("ntar aku traktir deh");
+    alert("udah lama ga meet up kan yaa");
+    alert("tapi kalo gabisa gapapaa kok, sanss");
+    alert("❤️❤️❤️");
+}
